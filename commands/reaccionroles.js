c3db41d5660603f7bfc1a5a7e7168e66d9846fee
Jossep <jossepparedes@gmail.com>
2021-01-13 04:10:40 -0400

    added role thief reaction

--- commands/reaccionroles.js
+++ commands/reaccionroles.js
@@ -9,16 +9,57 @@ function emoji (id) {
 }
 
 	const ThiefRole = message.guild.roles.cache.find(role => role.name === "Thief");
-    const thiefEmoji= emoji('798055358044176414');
+	const parerole = message.guild.roles.cache.find(role => role.name === "Thief");
+	const thiefEmoji= emoji('798055358044176414');
+	const thiefEmoji1= ('798055358044176414');
+	const pare = ('🛑');
 	
 	Embed
 	.setColor('RANDOM')
 	.setTitle('Selecciona un color\n')
-	.setDescription(`${thiefEmoji} sup`+' hola');
+	.setThumbnail("https://i.imgur.com/7bh8ur3.png")
+	.setDescription(`${thiefEmoji} Click en la reacción para aignar alguno de estos roles`);
 	
-
 	let messageEmbed = await message.channel.send(Embed);
 	messageEmbed.react(thiefEmoji);
+	messageEmbed.react(pare);
+	
+
+	client.on('messageReactionAdd', async (reaction, user) =>{
+		
+		if(reaction.message.partial) await reaction.message.fetch();
+		if(reaction.partial) await reaction.fetch();
+		if(user.bot) return;
+		if(!reaction.message.guild) return;
+
+		if(reaction.emoji.id === thiefEmoji1){
+			console.log("added");
+			await reaction.message.guild.members.cache.get(user.id).roles.add(ThiefRole);
+		console.log("added");
+		}
+		 else {return;}
+
+	});
+
+	
+	client.on('messageReactionRemove', async (reaction, user) =>{
+		
+		if(reaction.message.partial) await reaction.message.fetch();
+		if(reaction.partial) await reaction.fetch();
+		if(user.bot) return;
+		if(!reaction.message.guild) return;
+
+		if(reaction.emoji.id === thiefEmoji1){
+			console.log("added");
+			await reaction.message.guild.members.cache.get(user.id).roles.remove(ThiefRole);
+		console.log("added");
+		}
+		 else {return;}
+
+	});
+
+
+
 
 	}
 

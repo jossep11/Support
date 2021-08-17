88f5326c689a42ac2efd12abff9404f5aaa771ce
JossepP99 <jossepparedes1@gmail.com>
2021-08-17 00:04:30 -0400

    0.1.0 english version

--- commands/Words1c.js
+++ commands/Words1c.js
@@ -1,21 +1,26 @@
 const path = require("../data.json");
 const pagination = require('../pagination.js');
-const Spanish_logic = require('./data_fuctions');
+const data_english_spanish = require('./data_fuctions');
 
 module.exports = {
 	name: "Words1c",
 	description: "here is where the chatbot works!",
 	execute(message, args, contentss, Embed, client, Discord, ChannelIDs, distance) {
 		const palabras = path.contenido;
-        
+    
 //----------------------------------------*SPANISH const called from data_fuctions.js*-------------------------------------
 const [saludos, despedidas, recuperarID, recuperarClave, rated_Problema_Luancher, 
     rated_Conseguir_Mediador, rated_Pagina_VideoJuego, rated_Silk_Via_PayPal, rated_Subir_Nivel, rated_ComprarGold,
-    rated_guiaphanton, rated_guia_zerk_azul ] = Spanish_logic(contentss);    
+    rated_guiaphanton, rated_guia_zerk_azul ] = data_english_spanish.Spanish_logic(contentss);   
+    
+//----------------------------*ENGLISH const called from data_fuctions.js*-------------------------------------
+ const [greetings, goodbyes, recoverID, recoverPW, LauncherError, Find_Mediator, OficialWeb,
+        Silk_PayPal, Level_UP, BuyGold, Phantom_Guide, BlueZerk_Guide] = data_english_spanish.English_logic(contentss);   
 
 
 //conditional greeting--------------------------------------------------------------------------------------------------- 1
  if (saludos[0].distance >= 0.89 ) {
+     console.log(saludos[0].distance);
 let saludos_answer = palabras.Saludos;
 let respuestas = saludos_answer[1].respuestas;
 let aleatorio = Math.floor(Math.random() * respuestas.length);
@@ -92,7 +97,7 @@ else if(rated_Conseguir_Mediador[0].distance>=0.89){
     .setColor('#0099ff')
     .setTitle('Mediadores Oficiales')
     .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
-    .setDescription("En el videojuego puede contactar a cualquiera de los siguientes GM's: \n[GM]Celes \n[GM]Nao \n[GM]Scream \n[GM]Servivenet")
+    .setDescription("En el videojuego puede contactar a cualquiera de los siguientes mediadores: \n[GM]Celes \n[GM]Nao \n[GM]Scream \n[GM]Servivenet")
     .setTimestamp()
     message.author.send(Embed);
 }
@@ -180,6 +185,200 @@ else if(rated_guia_zerk_azul[0].distance>=0.89){
     message.author.send(Embed);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//conditional greeting--------------------------------------------------------------------------------------------------- 1
+else if (greetings[0].distance >= 0.89 ) {
+    console.log(greetings[0].distance);
+let greeting_answer = palabras.Greetings;
+let answers = greeting_answer[1].answers;
+let aleatorio = Math.floor(Math.random() * answers.length);
+
+message.author.send(answers[aleatorio]);
+const ChannelAuditx =client.channels.cache.find(channel=> channel.id=== "864924179043450901"); 
+
+} 
+
+//Condicional Despedida---------------------------------------------------------------------------------------------------- 2
+else if(goodbyes[0].distance>=0.89){
+    console.log(goodbyes[0].distance);
+    console.log("Running " + goodbyes[0].goodbye + "?");// i have to do it
+    }
+    
+    //Condicional RecuperarID--------------------------------------------------------------------------------------------------- 3
+    else if(recoverID[0].distance>=0.89 && contentss.includes('id') || recoverID[0].distance>=0.89 && contentss.includes('my use')){
+       // console.log(recuperarID[0].distance);
+        //console.log("Running " + recuperarID[0].recuperarid + "?");// 
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Recuperar ID')
+        .setURL('http://bit.ly/3rPFoN1')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .addField('Para recuperar ID:', 'En el apartado que puede visualizar debera ingresar su correo electrónico, comprobar que es un humano y luego de ello, se le enviará un correo recordándole cual es el ID asociado al correo: https://www.alfa.srolatino-servers.com/register/recoveryID')
+        .setImage('https://wiki.srolatino-servers.com/wp-content/uploads/2018/01/6.jpg')
+        .setTimestamp()
+        message.author.send(Embed);
+    
+    }
+    
+    //Condicional RecuperarClave--------------------------------------------------------------------------------------------------4
+    else if(recoverPW[0].distance>=0.96 && contentss.includes('my passw') ||recoverPW[0].distance>=0.96 && contentss.includes('passw') ){
+        //console.log(recuperarClave[0].distance);
+        //console.log("Running " + recuperarClave[0].recuperar_clave + "?");// 
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Recuperar PW')
+        .setURL('http://bit.ly/3rPFoN1')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .addField('Nota:', 'No podrás recuperar la clave si no sabes cuál es su respuesta secreta, para recuperar su clave secreta puede dirigirse a:')
+        .addField('Recuperar clave secreta','https://bit.ly/31W83F2')
+        .addField('Para recuperar la contraseña:', 'Debe ingresar el ID de su usuarios y su clave secreta, una vez que completamos el ID, contraseña secreta, el sistema nos enviará un email a nuestra casilla de correo asociada. Ahí nos facilitará un enlace para ingresar una nueva contraseña: https://www.alfa.srolatino-servers.com/register/recoveryPassword')
+        .setImage('https://wiki.srolatino-servers.com/wp-content/uploads/2018/01/7.jpg')
+        .setTimestamp()
+        message.author.send(Embed);
+    }
+    
+    //Problema con LUANCHER-------------------------------------------------------------------------------------------------------5
+    else if(LauncherError[0].distance>=0.89){
+        //console.log(rated_Problema_Luancher[0].distance);
+        //console.log("Running " + rated_Problema_Luancher[0].problemaluancher + "?");//
+        let clienterror = palabras.LauncherError;
+        let respuestas = clienterror[1].respuestas;
+        Embed
+        .setColor('RANDOM')
+        .setTitle('Solución de Launcher')
+        .setURL('https://wiki.srolatino-servers.com/faq/silkroad-online-launcher-dejo-de-funcionar/')
+        .setDescription(respuestas)
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setTimestamp()
+        .setImage('https://wiki.srolatino-servers.com/wp-content/uploads/2018/01/iOQ3O9C.jpg')
+        .setFooter('Silkroad Latino');
+    
+        message.author.send(Embed);
+    }
+    
+    //Conseguir un mediador----------------------------------------------------------------------------------- 6
+    else if(Find_Mediator[0].distance>=0.89){
+        //console.log(rated_Conseguir_Mediador[0].distance);
+        //console.log("Running " + rated_Conseguir_Mediador[0].conseguirMediador + "?");
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Mediadores Oficiales')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setDescription("En el videojuego puede contactar a cualquiera de los siguientes mediadores: \n[GM]Celes \n[GM]Nao \n[GM]Scream \n[GM]Servivenet")
+        .setTimestamp()
+        message.author.send(Embed);
+    }
+    
+    //Pagina oficial del videojuego----------------------------------------------------------------------------------- 7
+    else if(OficialWeb[0].distance>=0.87){
+        console.log(OficialWeb[0].distance);
+       // console.log("Running " + rated_Pagina_VideoJuego[0].PaginaVideoJuego + "?");// 
+       Embed
+       .setColor('#0099ff')
+       .setTitle('Página Oficial del Servidor')
+       .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+       .setDescription("-> https://www.alfa.srolatino-servers.com")
+       .setTimestamp()
+       message.author.send(Embed);
+    }
+    
+    //COMPRAR SILK VIA PAYPAL----------------------------------------------------------------------------------- 8
+    else if(Silk_PayPal[0].distance>=0.89){
+        //console.log(rated_Silk_Via_PayPal[0].distance);
+        //console.log("Running " + rated_Silk_Via_PayPal[0].Silk_PayPal + "?");// 
+        Embed
+       .setColor('#0099ff')
+       .setTitle('Silk Vía PayPal')
+       .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+       .setDescription("Puede comprarle silk vía PayPal a cualquier GM dentro del juego (con entrega inmediata) o comprar directamente en la web, pero esta puede demorar hasta 48 horas para entregar el producto")
+       .setTimestamp()
+       message.author.send(Embed);
+    }
+    
+    //COMO SUBIR DE NIVEL----------------------------------------------------------------------------------- 9
+    else if(Level_UP[0].distance>=0.89){
+        //console.log(rated_Subir_Nivel[0].distance);
+        //console.log("Running " + rated_Subir_Nivel[0].SubirNivel + "?");// 
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Subir de Nivel')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setDescription("Puede subir de nivel matando monstruos de niveles ligeramente más altos que usted o completando misiones dentro del videojeugo ")
+        .setTimestamp()
+        message.author.send(Embed);
+    
+    }
+    
+    //COMPRAR GOLD----------------------------------------------------------------------------------- 10
+    else if(BuyGold[0].distance>=0.89){
+        //console.log(rated_ComprarGold[0].distance);
+        //console.log("Running " + rated_ComprarGold[0].ComprarGold + "?");// i have to do it
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Comprar Oro')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setDescription("El comercio entre usuarios esta permitido y puede comprarle oro a cualquier usuario dentro del juego, solo asegúrese de siempre acudir a algun mediador oficial en el videojuego para que le ayude con su compra, así evitando posibles estafas")
+        .setTimestamp()
+        message.author.send(Embed);
+    
+    }
+    
+    //Guia Phanton----------------------------------------------------------------------------------- 11
+    else if(Phantom_Guide[0].distance>=0.89){
+        //console.log(rated_guiaphanton[0].distance);
+        //console.log("Running " + rated_guiaphanton[0].guia_phanton + "?");// i have to do it
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Guía Phantom')
+        .setURL('https://wiki.srolatino-servers.com/faq/quest-phantom-desert/')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setDescription("A phantom puede ingresar a partir del nivel 130 y dentro de la misma debe realizar 15 misiones para terminarlas \n")
+        .setTimestamp()
+        message.author.send(Embed);
+    }
+    
+    //Guia zerk azul----------------------------------------------------------------------------------- 12
+    else if(BlueZerk_Guide[0].distance>=0.89){
+        //console.log(rated_guia_zerk_azul[0].distance);
+        //console.log("Running " + rated_guia_zerk_azul[0].guia_zerkAzul + "?");// i have to do it
+        Embed
+        .setColor('#0099ff')
+        .setTitle('Guía de Zerk Azul')
+        .setURL('https://wiki.srolatino-servers.com/faq/titulos-knight-captain-baronet-general-baron-senior-general-count-chief-general/')
+        .setThumbnail('https://wiki.srolatino-servers.com/wp-content/uploads/2019/12/logowiki-1.png')
+        .setImage('http://4.bp.blogspot.com/-usb9Lv2FagM/Vm4EODFv1MI/AAAAAAAAAHQ/kQHtxmXckgg/s1600/blue_berserk_1.jpg')
+        .setDescription("Los niveles requeridos para realizar estas misiones son: \nLv 95\nLv 100\nLv 105\nLv 110")
+        .setTimestamp()
+        message.author.send(Embed);
+    }
+    
 
 
 
--- commands/data_fuctions.js
+++ commands/data_fuctions.js
@@ -1,5 +1,5 @@
 var distance = require('jaro-winkler');
-
+//SPANISH**************************************************************************************
 function Spanish_logic(contentss ) {
 
 
@@ -311,13 +311,347 @@ rated_guia_zerk_azul.sort(function(a, b) {
     }
 }); 
 //END Guia zerkazul
-
-
 return [rated_Saludo, rated_Despedida, rated_recuperarID, rated_recuperarClave, rated_Problema_Luancher, 
   rated_Conseguir_Mediador, rated_Pagina_VideoJuego, rated_Silk_Via_PayPal, rated_Subir_Nivel, rated_ComprarGold,
   rated_guiaphanton, rated_guia_zerk_azul];
-
-
 }
 
-  module.exports = Spanish_logic;
+
+
+
+
+
+
+
+
+
+
+
+//ENGLISH****************************************************************************************
+function English_logic(contentss) {
+  
+//Greetings----------------------------------------------------------------------------------- 1
+var greetings = ['hi', 'hey', 'hey there', 'you there', "what's up", 'hello', 'hi, can you help me with something', 'hi, can you help me', 'hello, can you help me', 'hello, i need some help', 'i need help', 'need help', 'help me', 'need some help', 'i need some help', 'hey i need some help', 'hey need some help'];
+var rated_Greetings = [];
+
+greetings.forEach(function(greeting) {
+  rated_Greetings.push({
+    greeting: greeting,
+    distance: distance(contentss, greeting, { caseSensitive: false })
+  });
+});
+
+rated_Greetings.sort(function(a, b) {
+if (a.distance < b.distance) {
+return 1;
+} else if (a.distance > b.distance) {
+  return -1;
+} else {
+ return 0;
+}
+}); 
+//END of greetings
+
+
+
+//Goodbyes ----------------------------------------------------------------------------------- 2
+var goodbyes = ['bye','see you', 'see you later', 'thanks', 'thank you', 'thank you so much'];
+var rated_Goodbyes    = [];
+
+goodbyes.forEach(function(goodbye) {
+    rated_Goodbyes.push({
+    goodbye: goodbye,
+    distance: distance(contentss, goodbye, { caseSensitive: false })
+    });
+});
+
+rated_Goodbyes.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END OF goodbyes
+
+
+
+//RECOVER ID----------------------------------------------------------------------------------- 3
+var recuperarID = ['hi, how can i recover my id?', 'hi, i need to recover my id', 
+'hi, i need to recover my user', 'i need to recover my id','recover my id', 'recover id', 
+'how can i recover my id', 'i forgot my id', 'forgot my id', 'need my id', 'hello, how can i recover my id', 
+'i forgot my id, how can i recover it', 'hey i need to recover my id', 'hello i forgot my id', 'recover my user id', 'i need to get my id'];
+var rated_recuperarID    = [];
+
+recuperarID.forEach(function(recuperarid) {
+    rated_recuperarID.push({
+    recuperarid: recuperarid,
+    distance: distance(contentss, recuperarid, { caseSensitive: false })
+    });
+});
+
+rated_recuperarID.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END OF RECOVER ID
+
+
+
+//RECUPERAR Clave----------------------------------------------------------------------------------- 4
+var recoverPassword = ['how can i recover my password','hi, how can i recover my password',
+'i need to recovery my password','recover my password', 'i need my password', 'hello i need my password', 'recover my pw', 
+'i forgot my pw', 'i forgot my password', 'i forgot password', 'how can i recover my password', 
+'i forgot my password, how can i recovery it', 'how i recover my password', 'how recover my password', 'how can i recover my pw', 'olvide mi clave', 'recover my password'];
+var rated_recoverPassword  = [];
+
+recoverPassword.forEach(function(recover_password) {
+    rated_recoverPassword.push({
+    recover_password: recover_password,
+    distance: distance(contentss, recover_password, { caseSensitive: false })
+    });
+});
+
+rated_recoverPassword.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END OF RECUPERAR Clave
+
+
+
+//LUANCHER error----------------------------------------------------------------------------------- 5
+var launcher_errors = ['i have some problems with my  launcher, how can i fix it','hi, i have some errors in my launcher',
+'im having problems with my launcher','my client dc me', 'i have some problems with the videogame', 'client error','i am having some problems with my launcher', 
+'launcher problems'];
+var rated_Launcher_Error  = [];
+
+launcher_errors.forEach(function(errorLauncher) {
+    rated_Launcher_Error.push({
+      errorLauncher: errorLauncher,
+    distance: distance(contentss, errorLauncher, { caseSensitive: false })
+    });
+});
+
+rated_Launcher_Error.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END LUANCHER
+
+
+
+
+//find a mediator----------------------------------------------------------------------------------- 6
+var Find_Mediator= ['where can i find a mediator?','how can i find a mediator', 'i need a mediator',
+'need a mediator','hi, i need a mediator', 'hello i need a mediator'];
+var rated_FindAMediator  = [];
+
+Find_Mediator.forEach(function(findMediator) {
+    rated_FindAMediator.push({
+      findMediator: findMediator,
+    distance: distance(contentss, findMediator, { caseSensitive: false })
+    });
+});
+
+rated_FindAMediator.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END MEDIATOR
+
+
+
+
+//official website----------------------------------------------------------------------------------- 7
+var Videogame_Web= ['hi what is the official website of the videogame','what is the official website of the videogame', 'how can i find the official website of the videogame',
+,'what is the website','website of the videogame', 'need the website'];
+var rated_Web  = [];
+
+Videogame_Web.forEach(function(website) {
+    rated_Web.push({
+      website: website,
+    distance: distance(contentss, website, { caseSensitive: false })
+    });
+});
+
+rated_Web.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END official website
+
+
+
+
+//BUY SILK BY PAYPAL----------------------------------------------------------------------------------- 8
+var Silk_By_PayPal= ['where can i buy silk by paypal','hi, where can i buy silk by paypal', 'where can i buy silk by paypal',
+'i need to buy silk by paypal', 'need to buy silk by paypal', 'need silk by paypal', 'silk by paypal'];
+var rated_Silk_By_PayPal  = [];
+
+Silk_By_PayPal.forEach(function(buy_silk) {
+    rated_Silk_By_PayPal.push({
+      buy_silk: buy_silk,
+    distance: distance(contentss, buy_silk, { caseSensitive: false })
+    });
+});
+
+rated_Silk_By_PayPal.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END SILK BY PAYPAL
+
+
+
+
+
+//How to level up----------------------------------------------------------------------------------- 9
+var Level_UP= ['¿how can i level up?','hi, how can i level up', 'how level up',
+'i need to level up', 'level up', 'need to level up faster'];
+var rated_LevelUp  = [];
+
+Level_UP.forEach(function(LevelUp) {
+    rated_LevelUp.push({
+      LevelUp: LevelUp,
+    distance: distance(contentss, LevelUp, { caseSensitive: false })
+    });
+});
+
+rated_LevelUp.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END LEVEL UP
+
+
+
+
+
+//BUYING GOLD----------------------------------------------------------------------------------- 10
+var BuyGold= ['who can i buy gold from?','hey i need to buy gold', 'i need to buy gold','who can i buy gold?',
+'where can i buy gold', 'who sells gold? '];
+var rated_buy_gold  = [];
+
+BuyGold.forEach(function(buy_gold) {
+    rated_buy_gold.push({
+      buy_gold: buy_gold,
+    distance: distance(contentss, buy_gold, { caseSensitive: false })
+    });
+});
+
+rated_buy_gold.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END BUYING GOLD
+
+
+
+
+//Guide Phantom----------------------------------------------------------------------------------- 11
+var phantom_guide= ['where can i get the phantom guide?','i need the phantom guide', 'need the phantom guide'
+, 'phantom guide'];
+var rated_guidephantom  = [];
+
+phantom_guide.forEach(function(phantom__guide) {
+  rated_guidephantom.push({
+      phantom__guide: phantom__guide,
+    distance: distance(contentss, phantom__guide, { caseSensitive: false })
+    });
+});
+
+rated_guidephantom.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END Guide Phantom
+
+
+
+
+//blue zerk guide----------------------------------------------------------------------------------- 12
+var BlueZerk_Guide= ['could you provide me with the blue zerk guide?','i need the blue zerk guide', 'blue zerk buide', 'need blue zerk guide'];
+var rated_Blue_Zerk_Guide  = [];
+
+BlueZerk_Guide.forEach(function(blue_zerkGuide) {
+    rated_Blue_Zerk_Guide.push({
+      blue_zerkGuide: blue_zerkGuide,
+    distance: distance(contentss, blue_zerkGuide, { caseSensitive: false })
+    });
+});
+
+rated_Blue_Zerk_Guide.sort(function(a, b) {
+    if (a.distance < b.distance) {
+    return 1;
+    } else if (a.distance > b.distance) {
+    return -1;
+    } else {
+    return 0;
+    }
+}); 
+//END blue zerk guide
+
+
+
+
+  
+return [rated_Greetings, rated_Goodbyes, rated_recuperarID,
+  rated_recoverPassword,
+  rated_Launcher_Error,
+  rated_FindAMediator,
+  rated_Web,
+  rated_Silk_By_PayPal,
+  rated_LevelUp,
+  rated_buy_gold,
+  rated_guidephantom,
+  rated_Blue_Zerk_Guide];
+}
+  module.exports = {Spanish_logic, English_logic};
